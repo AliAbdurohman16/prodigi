2b2b10b38f9d84652251afe799b06dc4f3134c98
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-16 20:47:25 +0700

    menambahkan migrasi

---
+++ database/migrations/2024_08_16_133742_create_settings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo_color');
+            $table->string('logo_white');
+            $table->string('title');
+            $table->string('desc');
+            $table->string('about_image_1');
+            $table->string('about_image_2');
+            $table->string('about_image_3');
+            $table->string('about_desc');
+            $table->string('happy_client');
+            $table->string('job_placement');
+            $table->string('project_complete');
+            $table->string('footer');
+            $table->string('email');
+            $table->string('telepon');
+            $table->string('whatsapp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
