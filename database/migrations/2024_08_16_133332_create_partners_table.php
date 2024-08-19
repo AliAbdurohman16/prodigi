9055aeb2577150d96cdb457a0622a1616222ab1c
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-19 08:50:15 +0700

    delete field width and height

--- database/migrations/2024_08_16_133332_create_partners_table.php
+++ database/migrations/2024_08_16_133332_create_partners_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('partners', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('image');
-            $table->string('width');
-            $table->string('height');
             $table->timestamps();
         });
     }
