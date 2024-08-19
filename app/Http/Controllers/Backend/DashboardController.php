df7ba98d37221e692032edcee3b4d18f13a4e5ef
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-19 22:14:15 +0700

    mendinamiskan dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Service;
+use App\Models\Product\Product;
+use App\Models\Portfolio\Post;
+use App\Models\Partner;
+use App\Models\Client;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard.index');
+        $data = [
+            'services' => Service::count(),
+            'products' => Product::count(),
+            'portfolios' => Post::count(),
+            'partners' => Partner::count(),
+            'clients' => Client::count(),
+        ];
+
+        return view('backend.dashboard.index', $data);
     }
 }
